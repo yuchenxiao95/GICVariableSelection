0304ebe67a6bb7b819429d58fa9b9d4b3fb30dcf
Jack Xiao <xiao.jack@utexas.edu>
2025-06-19 00:17:21 +0800

    Local updates to manifest/project files

--- test/Example.jl
+++ test/Example.jl
@@ -8,8 +8,8 @@ using GICVariableSelection, Plots, StatsBase, Distributions, DataFrames, LinearA
 
 
 Random.seed!(1234) 
-N, P, k = 5000, 100, 10
-rho = 0.1
+N, P, k = 5000, 500, 10
+rho = 0.0
 true_columns = sort(sample(1:P, k, replace=false))
 SNR = [0.09, 0.14, 0.25, 0.42, 0.71, 1.22, 2.07, 3.52, 6.00]
 
@@ -20,28 +20,37 @@ cov[i, i] = 1.0
 end
 X = Matrix(rand(MvNormal(mu, cov), N)')
 
-# Simulate multicolinearity
-# Choose 5 of them to be replaced
-num_replace = 3
-replace_columns = sample(true_columns, num_replace; replace = false)
-other_columns = setdiff(1:P, true_columns)
+# # Simulate multicolinearity
+# # Choose 5 of them to be replaced
+# num_replace = 3
+# replace_columns = sample(true_columns, num_replace; replace = false)
+# other_columns = setdiff(1:P, true_columns)
 
-# Replace each selected column with a linear combination of 2–7 other columns
-for col in replace_columns
-    # Pick 2–7 other columns randomly (excluding the current one)
-    other_cols = setdiff(other_columns, col)
-    mix_cols = sample(other_cols, rand(2:4); replace = false)
+# used_mix_cols = Vector{Vector{Int}}()  # This will be your list of lists
 
-    # Generate random coefficients for the linear combination
-    coeffs = 3*rand(length(mix_cols))  # or normalize if needed
+# used_cols = Set{Int}()  # Flat set of used columns to avoid reuse
 
-    # Replace the column with the linear combination
-    X[:, col] = X[:, mix_cols] * coeffs
-end
+# for col in replace_columns
+#     available = setdiff(setdiff(other_columns, [col]), collect(used_cols))
+
+#     if length(available) < 2
+#         @warn "Not enough columns left to mix for column $col"
+#         continue
+#     end
+
+#     mix = sample(available, rand(2:4); replace = false)
+#     coeffs = 3 * rand(length(mix))
+
+#     X[:, col] = X[:, mix] * coeffs
+
+#     push!(used_mix_cols, mix)
+#     union!(used_cols, mix)
+# end
+
 
 true_beta = zeros(P)
 true_beta[true_columns] .= 3
-variance = (true_beta' * cov * true_beta) / SNR[9]
+variance = (true_beta' * cov * true_beta) / SNR[4]
 std = sqrt(variance)
 
 Y = LP_to_Y(X, true_beta, family="Normal", std=std)
@@ -88,13 +97,10 @@ est_X =  X[:,tmp[2][end]]
 length(tmp[2][end])*log(tr(inv( X[:,tmp[2][end]]' * X[:,tmp[2][end]] ))/length(tmp[2][end])) - log(det(inv(X[:,tmp[2][end]]' * X[:,tmp[2][end]])))
 
 
-# 10*log(abs(tr(inv( X' * X )))/10) - logabsdet(inv(X' * X))[1]
-
-
-init_cols  = collect(1:P)
-#init_cols = sort(sample(1:P, Int64(floor(P/2)), replace=false))
+#init_cols  = collect(1:P)
+init_cols = sort(sample(1:P, Int64(floor(P/2)), replace=false))
 @time begin 
-tmp = GIC_Variable_Selection(X, Y, init_cols, Calculate_ICOMP, Calculate_ICOMP_short, Nsim=8)
+tmp = GIC_Variable_Selection(X, Y, init_cols, Calculate_SIC, Calculate_SIC_short, Nsim=6)
 end
 
 tmpp = DataFrame(A = tmp[1], 
